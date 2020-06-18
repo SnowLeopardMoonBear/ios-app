5ea873d3c45c4226352f29a4b77a201343cb6322
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-18 23:56:18 +0900

    Create New Project

---
+++ MyDream/MyDream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDream
+//
+//  Created by Park Changhoon on 2020/06/18.
+//  Copyright © 2020 Park Changhoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
