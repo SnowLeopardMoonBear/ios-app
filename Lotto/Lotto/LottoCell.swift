6d87cd101a1b5453b4b0b34f404e908d53dbc603
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-19 22:55:35 +0900

    Update: Lotto Cell Class(not working)
    1. cell이 담긴 row가 나타나지 않음. 컴파일 자체는 되는데...

---
+++ Lotto/Lotto/LottoCell.swift
@@ -0,0 +1,17 @@
+//
+//  LottoCell.swift
+//  Lotto
+//
+//  Created by Park Changhoon on 2020/06/19.
+//  Copyright © 2020 Park Changhoon. All rights reserved.
+//
+
+import UIKit
+class LottoCell: UITableViewCell {
+    @IBOutlet weak var number1: UILabel!
+    @IBOutlet weak var number2: UILabel!
+    @IBOutlet weak var number3: UILabel!
+    @IBOutlet weak var number4: UILabel!
+    @IBOutlet weak var number5: UILabel!
+    @IBOutlet weak var number6: UILabel!
+}
